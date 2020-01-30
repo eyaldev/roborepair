e0d01a02b3f58daf7d100ff9c0ce3ddf74399980
eshalev <eshalev@cisco.com>
2020-01-31 01:24:52 +0200

    basic platforming experiment

---
+++ roborepair/Assets/scripts/CharacterControllerBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerBehaviour : MonoBehaviour
+{
+    public int m_Acceleration=1000;
+    public int m_Jump = 400;
+    private Rigidbody2D m_RigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_RigidBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveForce = m_Acceleration * this.m_RigidBody.mass * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.m_RigidBody.AddForce(new Vector2(moveForce, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.m_RigidBody.AddForce(new Vector2(-moveForce, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            var jumpForce = m_Jump * this.m_RigidBody.mass; //not multipled by time because this is a burst
+            this.m_RigidBody.AddForce(new Vector2(0, jumpForce));
+        }
+
+    }
+}
